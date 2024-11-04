2c215dcbe54c1dd81c72a44a0c05a910a1fccd9f
ziid <ziid76@gmail.com>
2024-11-04 17:07:45 +0900

    '1.2'

--- secup/settings.py
+++ secup/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,16 +50,31 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://crosspeter.com",  # 요청을 허용할 도메인
-    'https://www.crosspeter.com',  # www 포함
-    'https://localhost:8080',
-        # 로컬 개발 시 필요
-]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
