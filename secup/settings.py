dad054938742c18710e59c0dd08455865032478b
ziid <ziid76@gmail.com>
2024-11-04 17:01:19 +0900

    '1.2'

--- secup/settings.py
+++ secup/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-*ae_!^!sw7@cu2j&0pzro@jliw(3v3rjfk5!vz6l-s##t2ic-@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', "crosspeter.com" ]
+ALLOWED_HOSTS = ['127.0.0.1', "crosspeter.com", '152.70.250.31' ]
 
 # Application definition
 
