ad04f5d11c1514a713aecc1b41a1ac0123e212b8
ziid <ziid76@gmail.com>
2025-02-12 13:06:35 +0900

    1.0

--- catalog/views/report_view.py
+++ catalog/views/report_view.py
@@ -4,6 +4,11 @@ def report_energy(request):
 
     return render(request, 'index.html')
 
+def report_energy_tab(request):
+
+    return render(request, 'report_energy_tab.html')
+
+
 def report_life(request):
 
     return render(request, 'report_life.html')
