4d09e3fedfadc3c5f63ccb8a2330ee7523a4b377
ziid <ziid76@gmail.com>
2025-02-12 10:38:22 +0900

    1.0

--- catalog/views/report_view.py
+++ catalog/views/report_view.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 
+def report_energy(request):
+
+    return render(request, 'index.html')
 
 def report_life(request):
 
     return render(request, 'report_life.html')
+
 def report_ev(request):
 
     return render(request, 'report_ev.html')
