2368dec8295ff4aabb3e5a85af9562aa97fc5466
ziid <ziid76@gmail.com>
2025-02-13 14:51:21 +0900

    1.0

--- catalog/views/report_view.py
+++ catalog/views/report_view.py
@@ -52,7 +52,7 @@ def report_dinning(request):
     for row in data:
         for key, value in row.items():
             if isinstance(value, dict) and value.get("specialValue") == "NaN":
-                row[key] = np.nan
+                row[key] = 0
 
     # 데이터프레임 생성
     df = pd.DataFrame(data)
@@ -69,30 +69,98 @@ def report_dinning(request):
     month_start_date = today.replace(day=1).strftime("%Y-%m-%d")
     search_date = "2025-02-06"
 
-    print(df[(df['date'] == search_date)]['chai_actual'])
-    print(df[(df['date'] == "2025-02-01")]['chai_actual'])
-    print(df[(df['date'] == "2025-02-02")]['chai_actual'])
-    print(df[(df['date'] == "2025-02-03")]['chai_actual'])
-    print(df[(df['date'] == "2025-02-04")]['chai_actual'])
-    print(df[(df['date'] == "2025-02-05")]['chai_actual'])
-
-
-
     # 기간 내 데이터 필터링 후 합산
-    y_b_d_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['chai_budget'].sum()
-    y_a_d_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['chai_actual'].sum()
-    m_b_d_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['chai_budget'].sum()
-    m_a_d_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['chai_actual'].sum()
-    d_b_d_sum = df[(df['date'] >= search_date) & (df['date'] <= search_date)]['chai_budget'].sum()
-    d_a_d_sum = df[(df['date'] >= search_date) & (df['date'] <= search_date)]['chai_actual'].sum()
+    # 차이797
+    y_b_c_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['chai_budget'].sum()
+    y_a_c_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['chai_actual'].sum()
+    m_b_c_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['chai_budget'].sum()
+    m_a_c_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['chai_actual'].sum()
+    d_b_c_sum = df[(df['date'] == search_date)]['chai_budget'].sum()
+    d_a_c_sum = df[(df['date'] == search_date)]['chai_actual'].sum()
+
+    # 정육점
+    y_b_j_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['j_budget'].sum()
+    y_a_j_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['j_actual'].sum()
+    m_b_j_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['j_budget'].sum()
+    m_a_j_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['j_actual'].sum()
+    d_b_j_sum = df[(df['date'] == search_date)]['j_budget'].sum()
+    d_a_j_sum = df[(df['date'] == search_date)]['j_actual'].sum()
+
+    # 서리재
+    y_b_s_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['s_budget'].sum()
+    y_a_s_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['s_actual'].sum()
+    m_b_s_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['s_budget'].sum()
+    m_a_s_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['s_actual'].sum()
+    d_b_s_sum = df[(df['date'] == search_date)]['s_budget'].sum()
+    d_a_s_sum = df[(df['date'] == search_date)]['s_actual'].sum()
+
+    # 호우섬
+    y_b_h_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['h_budget'].sum()
+    y_a_h_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['h_actual'].sum()
+    m_b_h_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['h_budget'].sum()
+    m_a_h_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['h_actual'].sum()
+    d_b_h_sum = df[(df['date'] == search_date)]['h_budget'].sum()
+    d_a_h_sum = df[(df['date'] == search_date)]['h_actual'].sum()
+
+    # 호우섬
+    y_b_i_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['i_budget'].sum()
+    y_a_i_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['i_actual'].sum()
+    m_b_i_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['i_budget'].sum()
+    m_a_i_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['i_actual'].sum()
+    d_b_i_sum = df[(df['date'] == search_date)]['i_budget'].sum()
+    d_a_i_sum = df[(df['date'] == search_date)]['i_actual'].sum()
 
     values ={
-        'd_b_d_sum': number_format(d_b_d_sum),
-        'd_a_d_sum': number_format(d_a_d_sum),
-        'm_b_d_sum': number_format(m_b_d_sum),
-        'm_a_d_sum': number_format(m_a_d_sum),
-        'y_b_d_sum': number_format(y_b_d_sum),
-        'y_a_d_sum': number_format(y_a_d_sum),
+        # 차이797
+        'd_b_c_sum': number_format(d_b_c_sum),
+        'd_a_c_sum': number_format(d_a_c_sum),
+        'd_c_per': d_a_c_sum/d_b_c_sum * 100,
+        'm_b_c_sum': number_format(m_b_c_sum),
+        'm_a_c_sum': number_format(m_a_c_sum),
+        'm_c_per': m_a_c_sum/m_b_c_sum * 100,
+        'y_b_c_sum': number_format(y_b_c_sum),
+        'y_a_c_sum': number_format(y_a_c_sum),
+        'y_c_per': y_a_c_sum/y_b_c_sum * 100,
+        # 정육점
+        'd_b_j_sum': number_format(d_b_j_sum),
+        'd_a_j_sum': number_format(d_a_j_sum),
+        'd_j_per': d_a_j_sum/d_b_j_sum * 100,
+        'm_b_ㅓ_sum': number_format(m_b_j_sum),
+        'm_a_j_sum': number_format(m_a_j_sum),
+        'm_j_per': m_a_j_sum/m_b_j_sum * 100,
+        'y_b_j_sum': number_format(y_b_j_sum),
+        'y_a_j_sum': number_format(y_a_j_sum),
+        'y_j_per': y_a_j_sum/y_b_j_sum * 100,
+        # 서리재
+        'd_b_s_sum': number_format(d_b_s_sum),
+        'd_a_s_sum': number_format(d_a_s_sum),
+        'd_s_per': d_a_s_sum/d_b_s_sum * 100,
+        'm_b_s_sum': number_format(m_b_s_sum),
+        'm_a_s_sum': number_format(m_a_s_sum),
+        'm_s_per': m_a_s_sum/m_b_s_sum * 100,
+        'y_b_s_sum': number_format(y_b_s_sum),
+        'y_a_s_sum': number_format(y_a_s_sum),
+        'y_s_per': y_a_s_sum/y_b_s_sum * 100,
+        # 호우섬
+        'd_b_h_sum': number_format(d_b_h_sum),
+        'd_a_h_sum': number_format(d_a_h_sum),
+        'd_h_per': d_a_h_sum/d_b_h_sum * 100,
+        'm_b_h_sum': number_format(m_b_h_sum),
+        'm_a_h_sum': number_format(m_a_h_sum),
+        'm_h_per': m_a_h_sum/m_b_h_sum * 100,
+        'y_b_h_sum': number_format(y_b_h_sum),
+        'y_a_h_sum': number_format(y_a_h_sum),
+        'y_h_per': y_a_h_sum/y_b_h_sum * 100,
+        # 일식
+        'd_b_i_sum': number_format(d_b_i_sum),
+        'd_a_i_sum': number_format(d_a_i_sum),
+        'd_i_per': d_a_h_sum/d_b_h_sum * 100,
+        'm_b_i_sum': number_format(m_b_i_sum),
+        'm_a_i_sum': number_format(m_a_i_sum),
+        'm_i_per': m_a_h_sum/m_b_h_sum * 100,
+        'y_b_i_sum': number_format(y_b_i_sum),
+        'y_a_i_sum': number_format(y_a_i_sum),
+        'y_i_per': y_a_h_sum/y_b_h_sum * 100,
     }
     
     return render(request, 'report_dinning.html', values)
