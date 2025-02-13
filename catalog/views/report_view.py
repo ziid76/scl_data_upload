975d60b542116a02755a9084c396e6ad74be4f04
ziid <ziid76@gmail.com>
2025-02-13 11:28:10 +0900

    1.0

--- catalog/views/report_view.py
+++ catalog/views/report_view.py
@@ -6,6 +6,7 @@ import os
 from pyairtable import Api
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 def report_energy(request):
 
@@ -32,8 +33,6 @@ def report_dinning(request):
     api = Api(token)
     table = api.table('appoYNBPdtDWEP3jr', 'SL&C')
     table.all(sort=["date"])
-    dataset = []
-
     for records in table.iterate(page_size=100, max_records=1000):
     #    dataset.append(records[2])
         print(records)
@@ -49,6 +48,35 @@ def report_dinning(request):
     # 데이터프레임 생성
     df = pd.DataFrame(data)
 
+    # date 필드를 datetime 형식으로 변환
+    df['date'] = pd.to_datetime(df['date'])
+
+    # 특정 기간 설정 (예: 2025-01-15 ~ 2025-01-30)
+    
+
+    # 오늘 날짜
+    today = datetime.today()
+
+    # 이번 달의 첫째 날
+    year_start_date = "2025-01-01"
+    month_start_date = today.replace(day=1)
+    search_date = "2025-02-6"
+    print(month_start_date.strftime("%Y-%m-%d"))  # YYYY-MM-DD 형식 출력
+
+
+    # 기간 내 데이터 필터링 후 합산
+    y_b_d_sum = df[(df['date'] >= year_start_date) & (df['date'] <= search_date)]['chai_budget'].sum()
+    m_b_d_sum = df[(df['date'] >= month_start_date) & (df['date'] <= search_date)]['chai_budget'].sum()
+    d_b_d_sum = df[(df['date'] >= search_date) & (df['date'] <= search_date)]['chai_budget'].sum()
+
+
+
     print(df)
-    return render(request, 'report_dinning.html', dataset)
+    values ={
+        'daily': d_b_d_sum,
+        'monthly': m_b_d_sum,
+        'yearly': y_b_d_sum,
+    }
+    
+    return render(request, 'report_dinning.html', values)
 
